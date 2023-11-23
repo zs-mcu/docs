f533378834e0f8d274aeac6734d26e6d35230789
zs <2020299870@qq.com>
2023-11-23 19:42:24 +0800

    zs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,15 +7,38 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'author', content: '小邵子' }],
-    ['meta', { name: 'keywords', content: '张邵,小邵子的 vuepress 个人博客' }]
+    ['meta', { name: 'keywords', content: '张邵,小邵子的 vuepress 个人博客' }],
+
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
+
+  base: "/docs/",
+
   plugins: [
+    //最后更新时间
     [
       '@vuepress/last-updated',
       {
         transformer: (timestamp, lang) => {
           //return moment(timestamp).fromNow()
           return moment(timestamp).format("LLLL")
+        }
+      }
+    ],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "网站有更新",
+          buttonText: "获取最新"
         }
       }
     ]
