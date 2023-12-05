bbf5e9d6a4eb65922b252095f1309ba0e0b97d9f
zs <2020299870@qq.com>
2023-12-05 15:23:26 +0800

    vssue

--- docs/.vuepress/config/pluginsConfig.js
+++ docs/.vuepress/config/pluginsConfig.js
@@ -2,50 +2,52 @@ const moment = require('moment');
 moment.locale('zh-cn')
 
 
-module.exports = [
-      ["vuepress-plugin-auto-sidebar", {
-        sort: {
-          // 更多选项: 
-          // `asc`、`desc`、`created_time_asc`、`created_time_desc`  | custom
-          // 在使用 created_time_asc 和 created_time_desc 必须使用 git (opens new window)跟踪文件。
-          // 自定义排序  mode: "custom"  fn: sortFn
-          mode: "desc",
-          //README文件一直在前面
-          readmeFirstForce: true
-          //所有文件会根据 autoSort 的大小进行排序，数值大的在前，数值小的在后，负数值会排在不具有 autoSort 属性的文件后。
-        },
-        sidebarDepth: 2,
-        collapse: {
-          open: true
-        },
-      }],
+module.exports = {
+  "vuepress-plugin-auto-sidebar": {
+    sort: {
+      // 更多选项: 
+      // `asc`、`desc`、`created_time_asc`、`created_time_desc`  | custom
+      // 在使用 created_time_asc 和 created_time_desc 必须使用 git (opens new window)跟踪文件。
+      // 自定义排序  mode: "custom"  fn: sortFn
+      mode: "desc",
+      //README文件一直在前面
+      readmeFirstForce: true
+      //所有文件会根据 autoSort 的大小进行排序，数值大的在前，数值小的在后，负数值会排在不具有 autoSort 属性的文件后。
+    },
+    sidebarDepth: 2,
+    collapse: {
+      open: true
+    },
+  },
+  '@vssue/vuepress-plugin-vssue': {
+    // 设置 `platform` 而不是 `api`
+    platform: 'github-v4',
 
-  
-      //最后更新时间
-      [
-        '@vuepress/last-updated',
-        {
-          transformer: (timestamp, lang) => {
-            //return moment(timestamp).fromNow()
-            return moment(timestamp).format("LLLL")
-          }
-        }
-      ],
-      [
-        '@vuepress/pwa',
-        {
-          serviceWorker: true,
-          updatePopup: {
-            message: "网站有更新",
-            buttonText: "获取最新"
-          }
-        }
-      ],
-      ['@vuepress/back-to-top'],
-      [
-        '@vuepress/google-analytics',
-        {
-          'ga': 'G-97XP980WZE' // UA-00000000-0
-        }
-      ]
-]
+    // 其他的 Vssue 配置
+    owner: 'zs-mcu',
+    repo: 'docs',
+    clientId: '09b48aa193526adeb1a8',
+    clientSecret: 'e3bfa36d814f2a4cc89ebefbb42858d714b4091e',
+    autoCreateIssue: true,
+    locale: 'zh'
+  },
+  //最后更新时间
+  '@vuepress/last-updated': {
+    transformer: (timestamp, lang) => {
+      //return moment(timestamp).fromNow()
+      return moment(timestamp).format("LLLL")
+    }
+  },
+  '@vuepress/pwa': {
+    serviceWorker: true,
+    updatePopup: {
+      message: "网站有更新",
+      buttonText: "获取最新"
+    }
+  },
+  '@vuepress/back-to-top': {},
+  '@vuepress/google-analytics': {
+    'ga': 'G-97XP980WZE' // UA-00000000-0
+  }
+
+}
